ee1a8d4a44063f4a4474d9dbdf2cb206666120b8
Chris Ninman <ctsnackpack@gmail.com>
2021-09-17 01:22:57 -0400

    added onclick reveals

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const headerOne = document.querySelector('h1')
+const revealManny = document.getElementById("section_three")
+const revealGuitar = document.getElementById("section_one")
+const revealPreK = document.getElementById("section_two")
+const revealThird = document.getElementById("section_four")
 
-console.log('h1 log', headerOne); 
+document.getElementById("manny").addEventListener("click", function() {
+    revealManny.style.display = "block";
+    revealGuitar.style.display = "none";
+    revealPreK.style.display = "none";  
+    revealThird.style.display = "none";   
+});
 
-headerOne.style.color = 'blue';
+
+document.getElementById("guitar").addEventListener("click", function() {
+    revealGuitar.style.display = "block";
+    revealManny.style.display = "none";
+    revealPreK.style.display = "none";  
+    revealThird.style.display = "none";  
+});
+
+document.getElementById("prek").addEventListener("click", function() {
+    revealPreK.style.display = "block";  
+    revealGuitar.style.display = "none";
+    revealManny.style.display = "none";
+    revealThird.style.display = "none";  
+});
+
+document.getElementById("third").addEventListener("click", function() {
+    revealThird.style.display = "block"; 
+    revealPreK.style.display = "none";  
+    revealGuitar.style.display = "none";
+    revealManny.style.display = "none"; 
+});
